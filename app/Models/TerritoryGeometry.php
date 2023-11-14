0ced6e44f316a8dd816b0f2b709f436d5d3a6884
Frank Hambach <frankhambach@erpe.de>
2023-11-14 21:40:50 +0100

    Added casts for geometric attributes of geometries

--- app/Models/TerritoryGeometry.php
+++ app/Models/TerritoryGeometry.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use MatanYadaev\EloquentSpatial\Objects\MultiLineString;
+use MatanYadaev\EloquentSpatial\Objects\MultiPolygon;
+use MatanYadaev\EloquentSpatial\Traits\HasSpatial;
 
 class TerritoryGeometry extends Model
 {
     use HasFactory;
+    use HasSpatial;
+
+    protected $casts = [
+        'land' => MultiPolygon::class,
+        'sea' => MultiPolygon::class,
+        'landBorder' => MultiLineString::class,
+        'seaBorder' => MultiLineString::class,
+        'coast' => MultiLineString::class,
+    ];
+
+    protected $fillable = ['land', 'sea', 'landBorder', 'seaBorder', 'coast'];
 
     public function territory()
     {
